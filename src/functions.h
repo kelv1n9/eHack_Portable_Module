35acb8e330d40aedede6818a4103e309e5126abd
Elvin Gadirov <93860175+kelv1n9@users.noreply.github.com>
2026-04-28 20:53:29 +0400

    Update app version to v1.5.4

--- src/functions.h
+++ src/functions.h
@@ -28,7 +28,7 @@
 #include "GyverOLED.h"
 
 #define APP_NAME "eHack Portable"
-#define APP_VERSION "v1.5.3"
+#define APP_VERSION "v1.5.4"
 
 #define DISABLE_DEVICE_PIN 22
 #define DISABLE_DEVICE_DELAY 180000 // ms
